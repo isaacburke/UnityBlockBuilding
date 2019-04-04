19b88065eafc7f1650405bedd1765824ea4ce1da
Isaac Burke <isaac.burke1@outlook.com>
2019-04-03 17:03:47 -0700

    Commit 1

--- Unity Project/Program.cs
+++ Unity Project/Program.cs
@@ -1,21 +1,49 @@
-using System;
+using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using UnityEngine;
 
-namespace Unity_Project
+public class GameController : MonoBehaviour
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
 
-            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+    // Use this for initialization
+    void Start()
+    {
+        for (int x = 0; x < 25; x++)
+        {
+            for (int z = 0; z < 25; x++)
+            {
+                CreateStack(x * 5, z * 5, 7);
+            }
         }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    void CreateStack(int x, int z, int numBlocks)
+    {
+        float start = 0.0f;
+        for (int i = 0; i < numBlocks; i++)
+        {
+            int height = 2, width = 2;
+            if (Random.Range(0.0f, 1.0f) < 0.75f)
+            {
+                height = 3;
+                width = 1;
+            }
+
+            float y = start + (0.5f * height);
+
+            GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            cube.AddComponent<Rigidbody>();
+            cube.transform.position = new Vector3(x, y, z);
+            cube.transform.localScale = new Vector3(width, height, width);
+
+            cube.GetComponent<Renderer>().material.color = new Color(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f), 1f);
+
+            start += height;
+        }
+
     }
 }
